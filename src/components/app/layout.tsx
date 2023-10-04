18c4519977642f05d2d8083f69b7b11674f3bd9d
onepercman <onepercman@gmail.com>
2023-10-04 11:33:27 +0700

    🚀 update project name

--- src/components/app/layout.tsx
+++ src/components/app/layout.tsx
@@ -30,7 +30,7 @@ export const Layout: FC<HTMLAttributes<HTMLDivElement>> = ({ children }) => {
             </div>
 
             <Drawer
-              title="Virgin"
+              title="Drawer"
               trigger={<Button leftIcon={<HiMenu />} variant="outlined" className="aspect-square p-0" />}
               className="flex flex-col gap-6 px-0"
             >
