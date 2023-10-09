df411365ca027ae65014608ea10b3ea2115042d4
onepercman <onepercman@gmail.com>
2023-10-09 14:52:25 +0700

    🚀 update axios config

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,34 @@
 import { API_URL } from "@/config/endpoints.config"
 import { useUser } from "@/models/root"
 import axios, { AxiosError, AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from "axios"
-import { camelizeKeys, decamelizeKeys } from "humps"
+
+const requestHandler = {
+  onFulfilled(config: InternalAxiosRequestConfig) {
+    const { token } = useUser.getState()
+    if (token) {
+      config.headers["Authorization"] = "Bearer " + token
+    }
+    return config
+  },
+}
+
+const responseHandler = {
+  onFulfilled(response: AxiosResponse) {
+    response.statusText = ""
+    response.data = response.data.data
+    return Promise.resolve(response)
+  },
+  onRejected(error: AxiosError) {
+    if (error.response?.data) {
+      const data = error.response.data as any
+      error.response.statusText = data["errorMsg"] || data["errorCode"]
+      error.response.data = null
+    } else {
+      error.response!.statusText = "Connection lost"
+    }
+    return Promise.resolve(error.response)
+  },
+}
 
 export const api: AxiosInstance = axios.create({
   baseURL: API_URL,
@@ -9,35 +36,5 @@ export const api: AxiosInstance = axios.create({
   headers: {},
 })
 
-function onRequestFulfilled(config: InternalAxiosRequestConfig) {
-  const { token } = useUser.getState()
-  if (token) {
-    config.headers["Authorization"] = "Bearer " + token
-  }
-  config.params = decamelizeKeys(config.params)
-  const contentType = config.headers["Content-Type"]?.toString() || ""
-  if (contentType.includes("json") || contentType.includes("text")) {
-    config.data = decamelizeKeys(config.data)
-  }
-  return config
-}
-
-function onResponseFulfilled(response: AxiosResponse) {
-  response.statusText = ""
-  response.data = camelizeKeys(response.data.data)
-  return Promise.resolve(response)
-}
-
-function onResponseRejected(error: AxiosError) {
-  if (error.response?.data) {
-    const data = camelizeKeys(error.response.data) as any
-    error.response.statusText = data["errorMsg"] || data["errorCode"]
-    error.response.data = null
-  } else {
-    error.response!.statusText = "Connection lost"
-  }
-  return Promise.resolve(error.response)
-}
-
-api.interceptors.request.use(onRequestFulfilled)
-api.interceptors.response.use(onResponseFulfilled, onResponseRejected)
+api.interceptors.request.use(requestHandler.onFulfilled)
+api.interceptors.response.use(responseHandler.onFulfilled, responseHandler.onRejected)
