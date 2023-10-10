4dde50b5ac24196896911efece2a882fbc96be7e
onepercman <onepercman@gmail.com>
2023-10-10 17:26:47 +0700

    🚀 remove string utils

--- src/utils/string.ts
+++
@@ -1,7 +0,0 @@
-import { Address } from "wagmi"
-
-export function truncateAddress(address: Address, numberic = 4) {
-  return (
-    address?.toLowerCase()?.slice(0, numberic > 2 ? numberic : 2) + "..." + address?.toLowerCase()?.slice(-numberic)
-  )
-}
