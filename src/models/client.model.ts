018d9ae346ff5e7e12ccf7c934d05c1cfa509a42
onepercman <onepercman@onepercmans-MacBook-Pro.local>
2023-12-15 15:31:20 +0700

    🚀 update useAddresses

--- src/models/client.model.ts
+++ src/models/client.model.ts
@@ -1,9 +1,10 @@
 import { defaultChain } from "@/config/chain.config"
 import { storageKeys } from "@/config/storage.config"
+import { ADDRESSES } from "@/constants/addresses"
 import { persistedProxy } from "@/libs/valtio"
 import { publicClient } from "@/libs/wagmi"
 import { useSnapshot } from "valtio"
-import { Chain, PublicClient, WalletClient } from "wagmi"
+import { Address, Chain, PublicClient, WalletClient } from "wagmi"
 
 class ClientModel {
   chain: Chain = defaultChain
@@ -11,6 +12,12 @@ class ClientModel {
 
   get publicClient(): PublicClient {
     return publicClient({ chainId: this.chain.id })
+  }
+
+  get addresses() {
+    return Object.fromEntries(
+      Object.entries(ADDRESSES).map(([key, object]) => [key, object[this.chain.id as keyof typeof object]]),
+    ) as Record<keyof typeof ADDRESSES, Address>
   }
 
   setWalletClient(walletClient: WalletClient | undefined) {
