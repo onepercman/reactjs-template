14aa331426ebd5c60246a2b86793ff51616ae5e7
onepercman <onepercman@onepercmans-MacBook-Pro.local>
2024-01-05 11:28:48 +0700

    🚀 update valtio persist lib

--- src/libs/valtio.ts
+++ src/libs/valtio.ts
@@ -1,7 +1,15 @@
+import { omit, pick } from "@/libs/object"
 import { proxy, subscribe } from "valtio"
 
 const defaultStorage = localStorage
-interface ProxyWithPersistOptions {
+
+interface WithInOrEx<T extends object> {
+  include?: KeyList<T>
+  exclude?: KeyList<T>
+}
+
+type KeyList<T extends object> = Array<keyof T>
+interface ProxyWithPersistOptions<T extends object> extends WithInOrEx<T> {
   key: string
   storage?: Storage
 }
@@ -14,11 +22,16 @@ function getPersist(key: string, storage: Storage) {
   }
 }
 
-function setPersist<T extends object>(key: string, state: T, storage: Storage) {
-  storage.setItem(key, JSON.stringify(state))
+function setPersist<T extends object>(key: string, state: T, storage: Storage, { exclude, include }: WithInOrEx<T>) {
+  try {
+    const data = include ? pick(state, include) : exclude ? omit(state, exclude) : state
+    storage.setItem(key, JSON.stringify(data))
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-function getMergedState<T extends object>(initialState: T, persistedState: T | null): T {
+function getMergedState<T extends object>(initialState: T, persistedState: T): T {
   const states = { ...initialState, ...persistedState }
   Object.setPrototypeOf(states, Object.getPrototypeOf(initialState))
   return states
@@ -26,13 +39,13 @@ function getMergedState<T extends object>(initialState: T, persistedState: T | n
 
 function proxyWithPersist<T extends object>(
   initialObject: T,
-  { key, storage = defaultStorage }: ProxyWithPersistOptions,
+  { key, storage = defaultStorage, exclude, include }: ProxyWithPersistOptions<T>,
 ): T {
   const mergedState = getMergedState(initialObject, getPersist(key, storage))
-  setPersist(key, mergedState, storage)
+  setPersist(key, mergedState, storage, { exclude, include })
   const state = proxy(mergedState)
   subscribe(state, () => {
-    setPersist(key, state, storage)
+    setPersist(key, state, storage, { exclude, include })
   })
   return state
 }
