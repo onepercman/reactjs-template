80b67bcb972b83075ff835264303245519f61eea
onepercman <onepercman@onepercmans-MacBook-Pro.local>
2024-01-08 17:11:22 +0700

    🚀 remove ga config

--- src/config/config.test.ts
+++ src/config/config.test.ts
@@ -1,7 +1,6 @@
 import { defaultChain } from "@/config/chain.config"
 import { API_URL } from "@/config/endpoints.config"
 import { ENV } from "@/config/env.config"
-import { GA_ID } from "@/config/ga.config"
 import { HOST } from "@/config/host.config"
 import { expect, test } from "vitest"
 
@@ -9,6 +8,5 @@ test("config", () => {
   expect(defaultChain).toBeTruthy()
   expect(ENV).toBeTruthy()
   expect(API_URL).toBeTruthy()
-  expect(GA_ID).toBeTruthy()
   expect(HOST).toBeTruthy()
 })
--- src/config/ga.config.ts
+++
@@ -1,9 +0,0 @@
-import { ENV, Env } from "./env.config"
-
-export const gaConfig = <const>{
-  [Env.Dev]: "",
-  [Env.Stg]: "",
-  [Env.Prd]: "",
-}
-
-export const GA_ID = gaConfig[ENV]
