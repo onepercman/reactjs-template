f5761d900b6eacef637cd6db68255022cdde3f11
onepercman <onepercman@onepercmans-MacBook-Pro.local>
2024-01-08 17:10:39 +0700

    🚀 update connectors

--- src/libs/wagmi.ts
+++ src/libs/wagmi.ts
@@ -1,21 +1,12 @@
-import { walletConnectId } from "@/config/wallet-connect.config"
 import { createClient, http } from "viem"
 import { createConfig } from "wagmi"
 import { goerli, mainnet } from "wagmi/chains"
-import { injected, walletConnect } from "wagmi/connectors"
 
 export const chains = <const>[mainnet, goerli]
 
 export enum ConnectorIds {
   Injected = "injected",
   WalletConnect = "walletconnect",
-}
-
-export const connectors = {
-  [ConnectorIds.Injected]: injected(),
-  [ConnectorIds.WalletConnect]: walletConnect({
-    projectId: walletConnectId,
-  }),
 }
 
 export const wagmiConfig = createConfig({
@@ -26,5 +17,4 @@ export const wagmiConfig = createConfig({
       transport: http(),
     })
   },
-  connectors: [connectors[ConnectorIds.Injected], connectors[ConnectorIds.WalletConnect]],
 })
