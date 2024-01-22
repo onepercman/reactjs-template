d26afd6f06dea87ce12efedfed96916837ade4b6
onepercman <onepercman@gmail.com>
2024-01-22 17:50:30 +0700

    🚀 update chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import react from "@vitejs/plugin-react-swc"
+import { randomUUID } from "crypto"
 import { defineConfig, loadEnv, splitVendorChunkPlugin } from "vite"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
 import vitePluginRadar from "vite-plugin-radar"
 import { default as viteTsConfigPaths } from "vite-tsconfig-paths"
+import { dependencies } from "./package.json"
+
+const globalVendorPackages = ["react", "react-dom", "react-router-dom"]
+
+function chunkDeps(deps: Record<string, string>) {
+  const chunks = {}
+  Object.keys(deps).forEach((key) => {
+    if (globalVendorPackages.includes(key)) return
+    chunks[randomUUID()] = [key]
+  })
+  return chunks
+}
 
 export default defineConfig(({ mode }) => ({
   base: loadEnv(mode, process.cwd()).VITE_PUBLIC_URL,
@@ -26,10 +39,13 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     minify: true,
-    chunkSizeWarningLimit: 3000,
+    sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: {},
+        manualChunks: {
+          vendor: globalVendorPackages,
+          ...chunkDeps(dependencies),
+        },
       },
     },
   },
