9583447c82a7ddd8524212008878405c08143b64
onepercman <onepercman@gmail.com>
2024-04-04 16:29:28 +0700

    🚀 update persist

--- src/libs/valtio.ts
+++ src/libs/valtio.ts
@@ -1,7 +1,7 @@
 import { proxy, subscribe, useSnapshot } from "valtio"
 import { omit, pick } from "./object"
 
-const defaultStorage = localStorage
+type StorageType = "localStorage" | "sessionStorage"
 
 interface WithInOrEx<T extends object> {
   include?: KeyList<T>
@@ -11,7 +11,7 @@ interface WithInOrEx<T extends object> {
 type KeyList<T extends object> = Array<keyof T>
 interface ProxyWithPersistOptions<T extends object> extends WithInOrEx<T> {
   key: string
-  storage?: Storage
+  storage?: StorageType
   onInit?(state: T): void
 }
 
@@ -20,22 +20,33 @@ type UseStoreReturnType<T extends object> = Pick<
   { [K in keyof T]: T[K] extends (...args: any) => any ? never : K }[keyof T]
 >
 
-function getPersist(key: string, storage: Storage) {
+function getStorage(type: StorageType): Storage | undefined {
+  if (typeof window !== "undefined") {
+    return window[type]
+  }
+  return undefined
+}
+
+function getPersistedData(key: string, storageType: StorageType) {
   try {
+    const storage = getStorage(storageType)
+    if (!storage) return {}
     return JSON.parse(storage.getItem(key) || "")
   } catch (err) {
-    console.log(`🔴 get persist error [${key}]`, err)
     return {}
   }
 }
 
-function setPersist<T extends object>(key: string, state: T, storage: Storage, { exclude, include }: WithInOrEx<T>) {
-  try {
-    const data = include ? pick(state, include) : exclude ? omit(state, exclude) : state
-    storage.setItem(key, JSON.stringify(data))
-  } catch (err) {
-    console.log(`🔴 set persist error [${key}]`, err)
-  }
+function persist<T extends object>(
+  key: string,
+  state: T,
+  storageType: StorageType,
+  { exclude, include }: WithInOrEx<T>,
+) {
+  const storage = getStorage(storageType)
+  if (!storage) throw new Error("Persist failed, Client not found")
+  const data = include ? pick(state, include) : exclude ? omit(state, exclude) : state
+  storage.setItem(key, JSON.stringify(data))
 }
 
 function getMergedState<T extends object>(initialState: T, persistedState: T): T {
@@ -44,17 +55,21 @@ function getMergedState<T extends object>(initialState: T, persistedState: T): T
   return states
 }
 
-function createStore<T extends object>(initialObject: T, persistOptions?: ProxyWithPersistOptions<T>): T {
-  if (!persistOptions) return proxy(initialObject)
-  const { key, storage = defaultStorage, exclude, include, onInit } = persistOptions
-  const mergedState = getMergedState(initialObject, getPersist(key, storage))
-  setPersist(key, mergedState, storage, { exclude, include })
+function createPersistStore<T extends object>(initialObject: T, persistOptions: ProxyWithPersistOptions<T>): T {
+  const { key, storage = "localStorage", exclude, include, onInit } = persistOptions
+  const mergedState = getMergedState(initialObject, getPersistedData(key, storage))
+  persist(key, mergedState, storage, { exclude, include })
   onInit && onInit(mergedState)
   const state = proxy(mergedState)
   subscribe(state, () => {
-    setPersist(key, state, storage, { exclude, include })
+    persist(key, state, storage, { exclude, include })
   })
   return state
+}
+
+function createStore<T extends object>(initialObject: T, persistOptions?: ProxyWithPersistOptions<T>): T {
+  if (!persistOptions) return proxy(initialObject)
+  return createPersistStore(initialObject, persistOptions)
 }
 
 function useStore<T extends object>(
