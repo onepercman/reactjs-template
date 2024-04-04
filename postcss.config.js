add97a20ffd4082b9c1a8189eaf4ec70146a26b0
onepercman <onepercman@gmail.com>
2024-04-04 16:36:50 +0700

    🚀 update base

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   plugins: {
     tailwindcss: { config: "./.tailwind/tailwind.config.ts" },
     autoprefixer: {},
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react-swc"
 import { randomUUID } from "crypto"
 import { defineConfig, loadEnv } from "vite"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
-import vitePluginRadar from "vite-plugin-radar"
+import { VitePluginRadar } from "vite-plugin-radar"
 import svgr from "vite-plugin-svgr"
 import { default as viteTsConfigPaths } from "vite-tsconfig-paths"
 import { dependencies } from "./package.json"
@@ -25,7 +25,7 @@ export default defineConfig(({ mode }) => ({
     react({ tsDecorators: true }),
     viteTsConfigPaths(),
     svgr({ include: "**/*.svg", svgrOptions: { icon: true, replaceAttrValues: { fill: "currentColor" } } }),
-    vitePluginRadar({ analytics: { id: loadEnv(mode, process.cwd()).VITE_GA_ID }, enableDev: true }),
+    VitePluginRadar({ analytics: { id: loadEnv(mode, process.cwd()).VITE_GA_ID }, enableDev: true }),
   ],
   server: { host: true, port: 3000 },
   build: {
