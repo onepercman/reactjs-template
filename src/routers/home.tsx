057c47a08b3427adbdc153807a2b55a59916f870
onepercman <onepercman@gmail.com>
2024-06-03 17:54:39 +0700

    🚀 update home

--- src/routers/home.tsx
+++ src/routers/home.tsx
@@ -1,13 +1,5 @@
-import { Button } from "@/libs/ui/button"
 import { Container } from "@/libs/ui/container"
-import { Tooltip } from "@/libs/ui/tooltip"
 
 export default function Home() {
-  return (
-    <Container>
-      <Tooltip content="This is a tooltip">
-        <Button>Tooltip</Button>
-      </Tooltip>
-    </Container>
-  )
+  return <Container></Container>
 }
