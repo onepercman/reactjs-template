5b8c1c1f0bb01b66205104401140b63654e1196e
onepercman <onepercman@gmail.com>
2024-06-04 14:32:04 +0700

    🚀 update home

--- src/routers/home.tsx
+++ src/routers/home.tsx
@@ -1,25 +1,5 @@
-import { Container, SegmentGroup } from "@/libs/one-ui/components"
+import { Container } from "@/libs/one-ui/components"
 
 export default function Home() {
-  return (
-    <Container>
-      <SegmentGroup
-        defaultValue="1"
-        items={[
-          {
-            children: "Item 1",
-            value: "1",
-          },
-          {
-            children: "Item 2",
-            value: "2",
-          },
-          {
-            children: "Item 3",
-            value: "3",
-          },
-        ]}
-      />
-    </Container>
-  )
+  return <Container></Container>
 }
