b574e097f2baf7f4dce037887233e39da823ebb4
onepercman <onepercman@gmail.com>
2024-06-07 15:20:10 +0700

    🚀 update mojaui configuration

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { VariantsProvider } from "@mojaui/react"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider } from "next-themes"
 import { RouterProvider } from "react-router-dom"
+import { variants } from "./config/variants"
 import { toaster } from "./libs/toast"
 import { router } from "./router"
 import "./styles/styles.scss"
@@ -11,7 +12,7 @@ export default function App() {
   return (
     <ThemeProvider defaultTheme="dark">
       <QueryClientProvider client={queryClient}>
-        <VariantsProvider toaster={toaster}>
+        <VariantsProvider customVariants={variants} toaster={toaster}>
           <RouterProvider router={router} />
         </VariantsProvider>
       </QueryClientProvider>
