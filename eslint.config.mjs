57e5738b7776c6e989fb53f7c2a70d1372f9d9a0
onepercman <onepercman@gmail.com>
2024-07-01 16:17:49 +0700

    🚀 update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+export default {
+  files: ["src/**/*.{js|jsx|ts|tsx}"],
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { ignoreRestSiblings: true },
+    ],
+  },
+}
