915594682e15ab8703d25b14d79dcf79f9fc7e96
onepercman ‎ <onepercman@192.168.1.32.non-exists.ptr.local>
2024-10-25 20:42:01 +0700

    🚀 upgrade routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,6 @@
-import { queryClient } from "@/libs/react-query"
-import { QueryClientProvider } from "@tanstack/react-query"
-import { ThemeProvider } from "next-themes"
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router"
 
 export default function App() {
-  return (
-    <ThemeProvider defaultTheme="dark">
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </ThemeProvider>
-  )
+  return <RouterProvider router={router} />
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+import { queryClient } from "@/libs/react-query"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { ThemeProvider } from "next-themes"
 import ReactDOM from "react-dom/client"
 import App from "./app"
 import "./styles/styles.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />,
+  <ThemeProvider defaultTheme="dark">
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </ThemeProvider>,
 )
