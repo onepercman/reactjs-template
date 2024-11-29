bad7fdbbce71e433b5d8f56aa36ee8666cfc8e19
onepercman <onepercman@gmail.com>
2024-11-29 09:55:21 +0700

    🚀 update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import { default as viteTsConfigPaths } from "vite-tsconfig-paths"
 
-export default defineConfig(({ mode }) => ({
-  base: loadEnv(mode, process.cwd()).VITE_PUBLIC_URL,
+export default defineConfig({
   plugins: [react({ tsDecorators: true }), viteTsConfigPaths()],
   server: { host: true, port: 3000 },
-}))
+})
