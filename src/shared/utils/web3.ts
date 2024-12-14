a7f3a284ce5f6fae8ac73b1fd44249f10fcbbe40
onepercman <onepercman@gmail.com>
2024-12-14 15:12:54 +0700

    🚀 clean web3 utils

--- src/shared/utils/web3.ts
+++
@@ -1,7 +0,0 @@
-export function truncateAddress(address: string, numberic = 4) {
-  return (
-    address?.toLowerCase()?.slice(0, numberic > 2 ? numberic : 2) +
-    "..." +
-    address?.toLowerCase()?.slice(-numberic)
-  )
-}
