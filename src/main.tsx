174ee27f2865e1523de75dd38edda23e20e69f22
onepercman <onepercman@gmail.com>
2025-06-16 16:48:56 +0700

    🚀 update web3 integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { queryClient } from "@/libs/react-query"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider } from "next-themes"
 import ReactDOM from "react-dom/client"
+import { WagmiProvider } from "wagmi"
 import App from "./app"
+import { wagmiAdapter } from "./config/web3.config"
 import { AutoUpdateVersion } from "./features/root/auto-update-version"
 import "./styles/styles.css"
 
@@ -26,8 +28,10 @@ if ("serviceWorker" in navigator) {
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ThemeProvider defaultTheme="dark">
     <QueryClientProvider client={queryClient}>
-      <App />
-      <AutoUpdateVersion />
+      <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+        <App />
+        <AutoUpdateVersion />
+      </WagmiProvider>
     </QueryClientProvider>
   </ThemeProvider>,
 )
